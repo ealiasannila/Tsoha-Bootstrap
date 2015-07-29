f7e27677e5afcbf8644f5921222f240815a321a1
Elias Annila <elias.annila@gmail.com>
2015-07-29 17:07:00 +0300

    html versioita hahmoteltu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,35 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on keskustelufoorumi';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+
+        View::make('helloworld.html');
     }
-  }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+    public static function register() {
+        View::make('suunnitelmat/register.html');
+    }
+    public static function newTopic() {
+        View::make('suunnitelmat/newTopic.html');
+    }
+    public static function newReply() {
+        View::make('suunnitelmat/newReply.html');
+    }
+    public static function subForum() {
+        View::make('suunnitelmat/subForum.html');
+    }
+    public static function forum() {
+        View::make('suunnitelmat/forum.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,22 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+  $routes->get('/register', function() {
+    HelloWorldController::register();
+  });
+  $routes->get('/newtopic', function() {
+    HelloWorldController::newTopic();
+  });
+  $routes->get('/newtopic/reply', function() {
+    HelloWorldController::newReply();
+  });
+  $routes->get('/subforum', function() {
+    HelloWorldController::subForum();
+  });
+  $routes->get('/forum', function() {
+    HelloWorldController::forum();
+  });
