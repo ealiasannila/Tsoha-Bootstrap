bee8401436947c553e42c3a23cb02e7b9b2f6db8
Elias Annila <elias.annila@gmail.com>
2015-08-02 22:56:47 +0300

    tietokantataulut ja html näkymiä

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,52 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE KayttajaRyhma(
+    id SERIAL PRIMARY KEY,
+    kuvaus varchar(100) NOT NULL
+);
+
+CREATE TABLE Aihealue(
+    id SERIAL PRIMARY KEY,
+    otsikko varchar(100) NOT NULL,
+    kuvaus varchar(100) NOT NULL
+);
+
+
+CREATE TABLE Aihe(
+    id SERIAL PRIMARY KEY,
+    aihealue INTEGER REFERENCES Aihealue(id) ON DELETE cascade ON UPDATE cascade NOT NULL
+);
+
+
+CREATE TABLE Kayttaja(
+    id SERIAL PRIMARY KEY,
+    kayttajatunnus varchar(50) NOT NULL,
+    salasana varchar(50) NOT NULL
+);
+
+
+CREATE TABLE Vastaus(
+    id SERIAL PRIMARY KEY,
+    otsikko varchar(100) NOT NULL,
+    teksti varchar(100) NOT NULL,
+    julkaistu TIMESTAMP,
+    laatija INTEGER REFERENCES Kayttaja(id) ON UPDATE cascade NOT NULL,
+    aihe INTEGER REFERENCES Aihe(id) ON DELETE cascade ON UPDATE cascade NOT NULL
+);
+
+CREATE TABLE Luettu(
+    kayttaja INTEGER REFERENCES Kayttaja(id) ON DELETE cascade ON UPDATE cascade NOT NULL,
+    vastaus INTEGER REFERENCES Vastaus(id) ON DELETE cascade ON UPDATE cascade NOT NULL
+);
+
+
+CREATE TABLE KayttajaKuuluu(
+    kayttaja INTEGER REFERENCES Kayttaja(id) ON DELETE cascade ON UPDATE cascade NOT NULL,
+    kayttajaRyhma INTEGER REFERENCES KayttajaRyhma(id) ON DELETE cascade ON UPDATE cascade NOT NULL
+);
+
+
+CREATE TABLE AihealueKuuluu(
+    aihealue INTEGER REFERENCES Aihealue(id) ON DELETE cascade ON UPDATE cascade NOT NULL,
+    kayttajaRyhma INTEGER REFERENCES KayttajaRyhma(id) ON DELETE cascade ON UPDATE cascade NOT NULL
+);
+
